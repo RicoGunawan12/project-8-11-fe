ab15d442f0f219a1dcefce428a760739bc56fa3e
Vionny <senvionny@gmail.com>
2024-12-29 04:01:29 +0700

    -

--- src/app/utilities/token.tsx
+++ src/app/utilities/token.tsx
@@ -1,11 +1,3 @@
-'use server'
-import { cookies } from "next/headers";
-
-export const setTokenCookie = async (token: string) => {
-  // document.cookie = `token=${token}; path=/; max-age=7200; secure; samesite=strict`;
-  
-  (await cookies()).set('token', token, {maxAge : 7200})
-};
 
 export const getTokenCookie = () => {
 
