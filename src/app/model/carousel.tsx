bd654b3d59abdd0d2a53013a800d5013e0469804
ZenX <zenxdiscord@gmail.com>
2025-01-08 23:10:33 +0700

    feature:connect_carousel

---
+++ src/app/model/carousel.tsx
@@ -0,0 +1,11 @@
+export interface CarouselItem {
+    carouselId: string;
+    carouselImage: string;
+    titleEng: string;
+    titleIndo: string;
+    contentEng: string;
+    contentIndo: string;
+    buttonEng: string;
+    buttonIndo: string;
+    link: string;
+  }
