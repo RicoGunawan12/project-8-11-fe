84006e3dbcae1ed50702517e51035d3ce1875e96
Rico <ricogunawan12323@gmail.com>
2025-02-18 08:25:49 +0700

    update: improve performance lcp

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,15 +6,16 @@ import React from 'react'
 // import NewCollection from './section/newCollection';
 // import Banner from './section/banner';
 import NavigationBar from '../component/navbar';
-import Footer from '../component/footer';
+// import Footer from '../component/footer';
 import dynamic from 'next/dynamic';
 
 
-const Banner = dynamic(() => import("./section/banner"))
-const BestSellerProduct = dynamic(() => import("./section/bestSellerProduct"))
-const NewCollection = dynamic(() => import("./section/newCollection"))
-const BestItem = dynamic(() => import("./section/bestItem"))
-const OurProductSection = dynamic(() => import("./section/ourProduct"))
+const Banner = dynamic(() => import("./section/banner"), { ssr: false })
+const BestSellerProduct = dynamic(() => import("./section/bestSellerProduct"), { ssr: false })
+const NewCollection = dynamic(() => import("./section/newCollection"), { ssr: false })
+const BestItem = dynamic(() => import("./section/bestItem"), { ssr: false })
+const OurProductSection = dynamic(() => import("./section/ourProduct"), { ssr: false })
+const Footer = dynamic(() => import("../component/footer"), { ssr: false })
 
 const HomePage = () => {
 
